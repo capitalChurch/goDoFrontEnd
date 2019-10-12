d2b2990a29e5d8216253e1c044d8bdf1f7722066
Kevyn Pinheiro Klava <kevynsax@gmail.com>
2019-10-11 23:17:56 -0300

    Second Test CI CD

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      tryint test CI CD
+      second test CI CD
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
