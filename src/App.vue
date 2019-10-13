50c6b9d43b1a92c1f62cca61e78d06ea999d8e98
Kevyn Pinheiro Klava <kevynsax@gmail.com>
2019-10-13 14:54:08 -0300

    Add fonts

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@
     @import "~vue-material/dist/theme/all";
 
     .app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        font-family: Biotif, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
@@ -51,5 +51,35 @@
                 width: 25px;
             }
         }
+    }
+
+    @font-face {
+        font-family: Biotif;
+        font-weight: 300;
+        src: local("Biotif Regular"), url("./assets/fonts/Biotif-Regular.ttf") format('truetype');
+    }
+
+    @font-face {
+        font-family: Biotif;
+        font-weight: 400;
+        src: local("Biotif Medium"), url("./assets/fonts/Biotif-Medium.ttf") format('truetype');
+    }
+
+    @font-face {
+        font-family: Biotif;
+        font-weight: 500;
+        src: local("Biotif Bold"), url("./assets/fonts/Biotif-Bold.ttf") format('truetype');
+    }
+
+    @font-face {
+        font-family: Biotif;
+        font-weight: 600;
+        src: local("Biotif Extra Bold"), url("./assets/fonts/Biotif-ExtraBold.ttf") format('truetype');
+    }
+
+    @font-face {
+        font-family: Biotif;
+        font-weight: 700;
+        src: local("Biotif Black"), url("./assets/fonts/Biotif-Black.ttf") format('truetype');
     }
 </style>
