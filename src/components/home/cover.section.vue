2b06e184268f07d1bed2245ed4557c2c8a1e2b0f
MenesesEvandro <MenesesEvandro@users.noreply.github.com>
2019-10-16 00:11:32 -0300

    Update cover.section.vue

--- src/components/home/cover.section.vue
+++ src/components/home/cover.section.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="coverSection">
         <ul class="cb-slideshow">
-            <li><span>Image 01</span><div><h3>Seja a<br />resposta para<br /> as necessidades do mundo.</h3></div></li>
-            <li><span>Image 02</span><div><h3>Seja a<br />resposta para<br /> as necessidades do mundo.</h3></div></li>
-            <li><span>Image 03</span><div><h3>Seja a<br />resposta para<br /> as necessidades do mundo.</h3></div></li>
-            <li><span>Image 04</span><div><h3>Seja a<br />resposta para<br /> as necessidades do mundo.</h3></div></li>
-            <li><span>Image 05</span><div><h3>Seja a<br />resposta para<br /> as necessidades do mundo.</h3></div></li>
-            <li><span>Image 06</span><div><h3>Seja a<br />resposta para<br /> as necessidades do mundo.</h3></div></li>
+            <li><span>&nbsp;</span><div><h3>Seja a<br />resposta para<br /> as necessidades do mundo.</h3></div></li>
+            <li><span>&nbsp;</span><div><h3>Seja a<br />resposta para<br /> as necessidades do mundo.</h3></div></li>
+            <li><span>&nbsp;</span><div><h3>Seja a<br />resposta para<br /> as necessidades do mundo.</h3></div></li>
+            <li><span>&nbsp;</span><div><h3>Seja a<br />resposta para<br /> as necessidades do mundo.</h3></div></li>
+            <li><span>&nbsp;</span><div><h3>Seja a<br />resposta para<br /> as necessidades do mundo.</h3></div></li>
+            <li><span>&nbsp;</span><div><h3>Seja a<br />resposta para<br /> as necessidades do mundo.</h3></div></li>
         </ul>
     </div>
 </template>
