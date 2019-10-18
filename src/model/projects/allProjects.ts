8bf88e63145369290cc944e4eea559de4c0f66c9
MenesesEvandro <MenesesEvandro@users.noreply.github.com>
2019-10-17 21:36:27 -0300

    Image Optimization

--- src/model/projects/allProjects.ts
+++ src/model/projects/allProjects.ts
@@ -2,9 +2,11 @@ import {project} from '@/model/projects/type';
 import {missionecas} from '@/model/projects/data/missionecas';
 import {haiti} from '@/model/projects/data/haiti';
 import {guine} from '@/model/projects/data/guine';
+import {cristolandia} from '@/model/projects/data/cristolandia';
 
 export const allProjects: project[] = [
     missionecas,
     haiti,
-    guine
+    guine,
+    cristolandia
 ];
