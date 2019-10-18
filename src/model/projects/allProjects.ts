99cb844831d85a5ecbb2373b8da2fd3e73410fe7
MenesesEvandro <MenesesEvandro@users.noreply.github.com>
2019-10-18 01:02:22 -0300

    Add Project Model
    Added Model for
    
    Espanha
    Refugiados
    Países Perseguidos

--- src/model/projects/allProjects.ts
+++ src/model/projects/allProjects.ts
@@ -1,7 +1,10 @@
 import {project} from '@/model/projects/type';
 import {missionecas} from '@/model/projects/data/missionecas';
 import {haiti} from '@/model/projects/data/haiti';
+import {espanha} from '@/model/projects/data/espanha';
 import {guine} from '@/model/projects/data/guine';
+import {refugiados} from '@/model/projects/data/refugiados';
+import {paisesperseguidos} from '@/model/projects/data/paisesperseguidos';
 import {realizandosonhos} from '@/model/projects/data/realizandosonhos';
 import {cristolandia} from '@/model/projects/data/cristolandia';
 import {ruah} from '@/model/projects/data/ruah';
@@ -11,7 +14,10 @@ import {reobote} from '@/model/projects/data/reobote';
 export const allProjects: project[] = [
     missionecas,
     haiti,
+    espanha,
     guine,
+    refugiados,
+    paisesperseguidos,
     realizandosonhos,
     cristolandia,
     ruah,
